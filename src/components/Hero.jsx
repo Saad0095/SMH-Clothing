b3985f8a065d2c10996cdd0d25b77fea9ee1eac4
Saad0095 <saadbinkhalid1895@gmail.com>
2024-09-26 15:25:34 +0500

    Created Carousel using react slicker

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Hero = () => {
-  return (
-    <div>
-      <img className='h-screen w-full bg-cover bg-center' src="../src/assets/Hero2.webp" alt=""/>
-    </div>
-  )
-}
+  const settings = {
+    dots: true,
+    lazyLoad: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    // cssEase: "linear",
+  };
 
-export default Hero
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <Slider {...settings}>
+        <div>
+          <img className="w-full h-screen object-cover" src="../src/assets/Hero1.webp" alt="Hero1" />
+        </div>
+        <div>
+          <img className="w-full h-screen object-cover" src="../src/assets/Hero2.webp" alt="Hero2" />
+        </div>
+        <div>
+          <img className="w-full h-screen object-cover" src="../src/assets/Hero3.webp" alt="Hero3" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Hero;
