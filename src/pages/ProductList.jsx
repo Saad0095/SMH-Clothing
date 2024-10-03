97b099eff9508b920a6c7793c578083cab600b0a
Saad0095 <saadbinkhalid1895@gmail.com>
2024-10-03 15:18:22 +0500

    Fixed some issues

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -6,39 +6,29 @@ import ProductCard from "./ProductCard";
 const ProductList = () => {
   const { section, subcategory } = useParams();
   const [products, setProducts] = useState([]);
-
+  
   useEffect(() => {
     const category = data.find(
       (category) => category.section.toLowerCase() === section.toLowerCase()
     );
     const subCat = category?.subcategories.find(
-      (subCategory) =>
-        subCategory.name.toLowerCase() === subcategory.toLowerCase()
+      (subCat) => subCat.name.toLowerCase() === subcategory.toLowerCase()
     );
 
-    if (subCat) {
-      setProducts(subCat.items);
-    }
-  }, [section, subcategory]);
+    setProducts(subCat.items);
+  }, []);
 
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">{subcategory}</h2>
-      <div className="flex flex-wrap justify-center mt-12">
-        {products.length > 0 ? (
-          products.map((item) => (
-            <Link key={item.id} to={`/${section}/${subcategory}/${item.id}`}>
-              <ProductCard
-                img={item.image}
-                name={item.name}
-                price={item.price}
-              />
-            </Link>
-          ))
-        ) : (
-          <div className="container mx-auto p-4">Subcategory not found</div>
-        )}
-      </div>
+    <div className="flex flex-wrap justify-center mx-auto my-20">
+      {products.length > 0 ? (
+        products.map((item) => (
+          <Link key={item.id} to={`/${section}/${subcategory}/${item.id}`}>
+            <ProductCard img={item.image} name={item.name} price={item.price} />
+          </Link>
+        ))
+      ) : (
+        <div className="container mx-auto p-4">Subcategory not found</div>
+      )}
     </div>
   );
 };
