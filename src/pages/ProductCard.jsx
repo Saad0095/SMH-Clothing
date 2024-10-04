dba526d4eaff54b80eacc2d964165bcd6a1d07f7
hussain hanif <hussaindr369@outlook.com>
2024-10-04 21:08:38 +0500

    Changed 1-2 images to test the issue in women's section

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({img,name,price}) => {
     
     <div className=' bg-white p-5 w-[315px] rounded-xl shadow-sm'>
       
-      <img className='w-full h-[350px] h-80 object-cover object-top drop-shadow[0_80px_30px_#0007] cursor-pointer hover:scale-105' src={img}alt=""/>
+      <img className='w-full h-full object-cover object-top drop-shadow[0_80px_30px_#0007] cursor-pointer hover:scale-105' src={img}alt=""/>
       <div>
         <h2 className='text-2x1 py-3 text-xs'>{name}</h2>
       </div>
