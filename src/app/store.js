20ee4114acdf7816bd7527ae55e5c01cde016e0f
Ibad moin <ibadmoin@gmail.com>
2024-10-05 14:07:01 -0700

    Add persist functionality for cart state

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./cartSlice";
-import productSlice from "./productSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import productReducer from './productSlice';
+import cartReducer from './cartSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedCartReducer = persistReducer(persistConfig, cartReducer);
 
 const store = configureStore({
   reducer: {
-      cart: cartSlice,
-      product: productSlice
+    products: productReducer,
+    cart: persistedCartReducer,
   },
 });
 
+export const persistor = persistStore(store);
 export default store;
