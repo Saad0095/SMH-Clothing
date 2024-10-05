6db7df229131b2b4fca577bfac8f052df9d98629
Muhib Sami <muhib.sami.pk@gmail.com>
2024-10-05 22:31:09 +0500

    Redux cartSlice and productSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
+import productSlice from "./productSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+      cart: cartSlice,
+      product: productSlice
+  },
 });
 
 export default store;
