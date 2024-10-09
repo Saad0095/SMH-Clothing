e4f354dfc5f38ca2c8b3d5d654ed47cb9c31dad6
Saad0095 <saadbinkhalid1895@gmail.com>
2024-10-09 23:42:46 +0500

    Added Pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Pagination = ({
+  totalPosts,
+  postsPerPage,
+  currentPage,
+  setCurrentPage,
+}) => {
+  const pages = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pages.push(i);
+  }
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    window.scrollTo({ top: 0, left: 0 });
+  };
+
+  return (
+    <div className="p-4 flex gap-4">
+      {pages.map((page, index) => {
+        return (
+          <button
+            className={`text-xl border-2 border-cyan-500 px-4 py-1 cursor-pointer transition-all ${
+              page === currentPage ? "bg-cyan-500 font-bold text-white" : " text-cyan-500"
+            }`}
+            key={index}
+            onClick={() => handlePageChange(page)}
+          >
+            {page}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Pagination;
--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -6,7 +6,6 @@ const ScrollToTop = () => {
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0 });
-    console.log(pathname);
   }, [pathname]);
 };
 
