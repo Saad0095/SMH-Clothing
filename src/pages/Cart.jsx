3c9fe824db9824bdb1067ce84fe9fd0a70a0dda4
hussain hanif <hussaindr369@outlook.com>
2024-10-09 09:03:26 +0500

    Completed styling for 'Add to Cart'

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -16,19 +16,21 @@ const Cart = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="border-t pt-14">
+    <div className="border-t pt-14 ">
 
       <div>
       {cart.cartItems.length > 0 ? (
-            cart.cartItems.map((product) => (
-              <div key={product.id} className="py-4  border-t border-b text-gray-700 items-center grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr]  gap-4">
-                <div className="flex items-start gap-6">
-                  <img className="w-16 sm:w-20" src={product.image[0]} alt="" />
+        <>
+            {cart.cartItems.map((product) => (
+              
+              <div key={product.id} className="py-4 container   sm:mx-4   border-t border-b text-gray-700  items-center grid  grid-cols-[2fr_0.5fr_0.5fr] sm:grid-cols-[2fr_2fr_0.5fr]  gap-4">
+                <div className="flex items-start  sm:gap-6">
+                  <img className="w-16 sm:w-20 mx-4 " src={product.image[0]} alt="" />
                   <div>
                   <p className="text-xs sm:text-lg font-medium">{product.name}</p>
-                  <div className="flex items-center gap-5 mt-2 ">
+                  <div className="flex items-center sm:gap-5 gap-2 mt-2 ">
                     
-                    <p className="text-lg-3xl font-semi-bold">
+                    <p className="sm:text-lg text-sm font-semi-bold">
                            Rs{" "}
                            {parseFloat(
                              product.price.replace(/,/g, "")
@@ -39,7 +41,7 @@ const Cart = () => {
                   </div>
                   </div>
                 </div>
-                <div className="flex items-center justify-center ">
+                <div className="flex items-center justify-center sm:mt-0  mt-5 ">
                          <button
                            className="max-w-10 sm:max-w-20 px-1 sm:px-2 py-1 border-r"
                            onClick={() =>
@@ -48,7 +50,7 @@ const Cart = () => {
                          >
                            -
                          </button>
-                         <p className="text-xl px-2">{product.quantity}</p>
+                         <p className="sm:text-xl  text-sm sm:px-2  px-1 ">{product.quantity}</p>
                          <button
                                  className="max-w-10 sm:max-w-20 px-1 sm:px-2 py-1 border-l"
 
@@ -59,7 +61,7 @@ const Cart = () => {
                            +              
                          </button>
                          <button
-                         className="text-red-500 hover:text-red-700 w-4 mr-4 sm:w-5 cursor-pointer "
+                         className="text-red-500  text-sm hover:text-red-700 w-4 mr-4 sm:w-5 cursor-pointer "
                          onClick={() =>
                            dispatch(removeFromCart({ id: product.id }))
                          }
@@ -68,7 +70,7 @@ const Cart = () => {
                        </button>
                        
                        </div>
-                       <p className=" font-bold">
+                       <p className=" font-semibold sm:text-xl text-sm sm:mt-0 mt-5">
                         Rs{" "}
                         {(
                           parseFloat(product.price.replace(/,/g, "")) *
@@ -76,140 +78,66 @@ const Cart = () => {
                         ).toLocaleString()}
                       </p>
 
-
-              </div>
-            ))
-          ) : (
-            <p>no product in the cart </p>
+                     
+              </div> 
+             
+            ))}
+            <div className="flex justify-end m-16">
+            <div className=" w-full sm:w-[450px]  p-6  rounded-lg shadow-md border  ">
+               <h3 className="text-sm font-semibold mb-5">CART TOTAL</h3>
+               <div className="flex  justify-between mb-5 border-b pb-1">
+                 <span className="text-sm">TOTAL ITEMS:</span>
+                 <span>
+                   {cart.cartItems.reduce(
+                     (totalQuantity, product) =>
+                       totalQuantity + product.quantity,
+                     0
+                   )}
+                 </span>
+               </div>
+               <div className="flex justify-between mb-4">
+                 <span>TOTAL PRICE:</span>
+                 <span>
+                   Rs{" "}
+                   {cart.cartItems
+                     .reduce(
+                       (total, product) =>
+                         total +
+                         parseFloat(product.price.replace(/,/g, "")) *
+                           product.quantity,
+                       0
+                     )
+                     .toLocaleString()}
+                 </span>
+               </div>
+               <button
+                 className="w-full bg-red-600 mb-2 text-white py-2 hover:bg-red-800"
+                 onClick={() => navigate("/checkout")}
+               >
+                 Checkout
+               </button>
+               <button
+                 className="w-full bg-white text-black py-2 hover:text-white hover:bg-red-800  border border-black"
+                 onClick={() => navigate("/")}
+               >
+                 CONTINUE SHOPPING
+               </button>  
+             </div>
+             </div>
+             
+            </>
+          )
+          
+           : (
+            <div className="flex justify-center">
+                  <img src={EmptyCart} alt="Empty Cart" className="h-96" />
+                </div>
           )}
       </div>
 
 
     </div>
-
-    // <div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24 pt-28">
-    //   {cart.cartItems.length > 0 ? (
-    //     <div className="py-10">
-    //       <h3 className="text-2xl font-semibold mb-4">SHOPPING CART</h3>
-    //       <div className="flex flex-col md:flex-row justify-between space-x-10 mt-8 gap-4">
-    //         <div className="w-[90%] md:w-2/3">
-    //           <div className="flex justify-between border-b items-center mb-4 text-us font-bold">
-    //             <p>PRODUCT</p>
-    //             <div className="flex space-x-8">
-    //               <p>PRICE</p>
-    //               <p>QUANTITY</p>
-    //               <p>SUBTOTAL</p>
-    //               <p>REMOVE</p>
-    //             </div>
-    //           </div>
-    //           {cart.cartItems.map((product) => (
-    //             <div
-    //               key={product.id}
-    //               className="flex items-center justify-between p-3 border-b"
-    //             >
-    //               <div className="md:flex items-center space-x-4">
-    //                 <div>
-    //                   <img
-    //                     src={product.image[0]}
-    //                     alt={product.name}
-    //                     className="w-16 h-16 object-contain rounded"
-    //                   />
-    //                 </div>
-    //                 <div className="flex space-x-14 items-center">
-    //                   <div className="flex-1">
-    //                     <h3 className="text-lg font-semibold">
-    //                       {product.name}
-    //                     </h3>
-    //                   </div>
-    //                   <div className="priceContainer">
-    //                     <p className="text-lg-3xl font-semi-bold">
-    //                       Rs{" "}
-    //                       {parseFloat(
-    //                         product.price.replace(/,/g, "")
-    //                       ).toLocaleString()}
-    //                     </p>
-    //                   </div>
-    //                   <div className="flex items-center justify-center border">
-    //                     <button
-    //                       className="text-xl font-bold px-1.5 border-r"
-    //                       onClick={() =>
-    //                         dispatch(decreaseQuantity({ id: product.id }))
-    //                       }
-    //                     >
-    //                       -
-    //                     </button>
-    //                     <p className="text-xl px-2">{product.quantity}</p>
-    //                     <button
-    //                       className="text-xl font-bold px-1.5 border-l"
-    //                       onClick={() =>
-    //                         dispatch(increaseQuantity({ id: product.id }))
-    //                       }
-    //                     >
-    //                       +
-    //                     </button>
-    //                   </div>
-    //                   <p>
-    //                     Rs{" "}
-    //                     {(
-    //                       parseFloat(product.price.replace(/,/g, "")) *
-    //                       product.quantity
-    //                     ).toLocaleString()}
-    //                   </p>
-    //                   <button
-    //                     className="text-red-500 hover:text-red-700"
-    //                     onClick={() =>
-    //                       dispatch(removeFromCart({ id: product.id }))
-    //                     }
-    //                   >
-    //                     <FontAwesomeIcon icon={faTrashAlt} />
-    //                   </button>
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           ))}
-    //         </div>
-    //         <div className="md:w-1/3 p-6 rounded-lg shadow-md border">
-    //           <h3 className="text-sm font-semibold mb-5">CART TOTAL</h3>
-    //           <div className="flex justify-between mb-5 border-b pb-1">
-    //             <span className="text-sm">TOTAL ITEMS:</span>
-    //             <span>
-    //               {cart.cartItems.reduce(
-    //                 (totalQuantity, product) =>
-    //                   totalQuantity + product.quantity,
-    //                 0
-    //               )}
-    //             </span>
-    //           </div>
-    //           <div className="flex justify-between mb-4">
-    //             <span>TOTAL PRICE:</span>
-    //             <span>
-    //               Rs{" "}
-    //               {cart.cartItems
-    //                 .reduce(
-    //                   (total, product) =>
-    //                     total +
-    //                     parseFloat(product.price.replace(/,/g, "")) *
-    //                       product.quantity,
-    //                   0
-    //                 )
-    //                 .toLocaleString()}
-    //             </span>
-    //           </div>
-    //           <button
-    //             className="w-full bg-red-600 text-white py-2 hover:bg-red-800"
-    //             onClick={() => navigate("/checkout")}
-    //           >
-    //             Checkout
-    //           </button>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   ) : (
-    //     <div className="flex justify-center">
-    //       <img src={EmptyCart} alt="Empty Cart" className="h-96" />
-    //     </div>
-    //   )}
-    // </div>
+    
   );
 };
 
@@ -217,81 +145,4 @@ export default Cart;
 
 
 
-{/* <div className="py-8 max-w-8xl container mx-auto px-3 mt-5">
-      <h2 className="mb-5 text-xl font-bold">Shopping Cart</h2>
-      <div className="flex flex-col md:flex-row justify-between md-gap-8 gap-4 ">
-        <div className="space-y-6 ">
-          {cart.cartItems.length > 0 ? (
-            cart.cartItems.map((product) => (
-              <div key={product.id} className="rounded-lg">
-                <div className="justify-between mb-6 rounded-lg  bg-white p-6 shadow-md flex justify-start ">
-                  <img
-                    src={product.image[0]}
-                    className="w-full h-28 rounded-lg w-20 "
-                    alt=""
-                  />
-                  <div className="ml-4 flex w-full justify-between">
-                    <div className=" mt-0">
-                      <h2 className="text-sm font-bold text-gray-900">
-                        {product.name}
-                      </h2>
-                      <p className="mt-1 text-sm text-gray-900">
-                        Rs{" "}
-                        {parseFloat(
-                          product.price.replace(/,/g, "")
-                        ).toLocaleString()}
-                      </p>
-                    </div>
-                    <div className=" flex justify-between mb-24 space-y-4 block space-x-6">
-                      <div className="flex items-center  border-gray-100">
 
-                      <button
-                         className="text-xl  px-1.5 border"
-                         onClick={() =>
-                           dispatch(decreaseQuantity({ id: product.id }))
-                         }
-                       >
-                         -
-                       </button>
-                       <p className="text-xl px-2 border">{product.quantity}</p>
-                       <button
-                         className="text-xl  px-1.5 border"
-                         onClick={() =>
-                           dispatch(increaseQuantity({ id: product.id }))
-                         }
-                       >
-                         +
-                       </button>
-                       <button
-                         className="text-red-500 ml-2 hover:text-red-700"
-                         onClick={() =>
-                           dispatch(removeFromCart({ id: product.id }))
-                         }
-                       >
-                         <FontAwesomeIcon icon={faTrashAlt} />
-                       </button>
-                      </div>
-                     
-
-                    </div>
-                    
-                    <div className=" mt-1 flex justify-between">
-                      <p className=" font-bold">
-                        Rs{" "}
-                        {(
-                          parseFloat(product.price.replace(/,/g, "")) *
-                          product.quantity
-                        ).toLocaleString()}
-                      </p>
-                      </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p>no product in the cart </p>
-          )}
-        </div>
-        <div className="md-w-1/3"> billing</div>
-      </div>
-    </div> */}
