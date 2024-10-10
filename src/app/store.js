faa02f5ca3bc8373e8e541eb3a630c63eb3e44c3
Saad0095 <saadbinkhalid1895@gmail.com>
2024-10-11 00:20:39 +0500

    Deleted productSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import productReducer from './slices/productSlice';
 import cartReducer from './slices/cartSlice';
 import orderReducer from './slices/orderSlice';
 
 export const store = configureStore({
   reducer: {
-    products: productReducer,
     cart: cartReducer,
     order: orderReducer,
   },
