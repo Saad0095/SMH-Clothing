9957d992d3636aaea8a522154cc08a20ce7a4a08
hussain hanif <hussaindr369@outlook.com>
2024-10-16 06:51:49 +0500

    Added sorting functionality for products

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import productReducer from './slices/productSlice';
 import cartReducer from './slices/cartSlice';
 import orderReducer from './slices/orderSlice';
 
 export const store = configureStore({
   reducer: {
-    products: productReducer,
+   
     cart: cartReducer,
     order: orderReducer,
+    
   },
 });
 
